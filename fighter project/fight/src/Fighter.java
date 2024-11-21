a5a747e6d9d95b5e9410773ff070fa467e2f4dee
Emre Şahin <147410835+sahineemree@users.noreply.github.com>
2024-11-21 14:27:09 +0300

    Add files via upload

---
+++ fighter project/fight/src/Fighter.java
@@ -0,0 +1,41 @@
+public class Fighter {
+   String name;
+   int damage;
+   int health;
+   int weight;
+   int dodge;
+   Fighter(String name, int damage, int health, int weight, int dodge) {
+
+      this.name = name;
+      this.damage = damage;
+      this.health = health;
+      this.weight = weight;
+      if (dodge<=100 && dodge >=0){
+         this.dodge = dodge;
+      }
+      else {
+         this.dodge=0;
+      }
+
+
+   }
+   int hit (Fighter foe){
+      if (foe.isDodge()){
+         System.out.println(foe.name + " adlı oyuncu gelen hasarı blokladı !");
+         System.out.println("----------");
+         return foe.dodge;
+      }
+
+      System.out.println(this.name + " => " + foe.name + " oyuncusuna " + this.damage + " hasar vurdu. ");
+
+      if (foe.health - this.damage <0){
+         return 0;
+      }
+
+      return foe.health - this.damage ;
+   }
+   boolean isDodge (){
+      double randomNumber = Math.random()*100;
+      return randomNumber <= this.dodge;
+   }
+}
